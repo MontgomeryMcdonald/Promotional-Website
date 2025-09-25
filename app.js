dd84350e5740b6dcc4d48d88ad88c755a1f3e25b
Montgomery Mcdonald <halfbloodhero1027@gmail.com>
2025-09-25 15:08:46 -0700

    Further Progress

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import {fileURLToPath} from 'url'
 import {ensureDataFile, listData, addData} from './utils/api.js'
 
 
+let ID = ""
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -43,6 +45,81 @@ app.post("/api/data", async (req, res, next) => {
     }
 })
 
+let username = "Monte123"
+let password = "abc123supersecret"
+
+app.post("/api/login", async (req, res, next) => {
+    try {
+        const data = req.body
+        console.log("try reached")
+        if(data.username == username && data.password == password){
+            console.log("tests passed. sending data")
+            ID = (Date.now().toString(36)+Math.random().toString(36).slice(2,8).toUpperCase())
+            res.redirect(`http://localhost:5000/${ID}`)
+        }else{
+            console.log("tests failled")
+            res.redirect("http://localhost:5000/")
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
+async function compileData(){
+    let data = await listData()
+    let output = ""
+    for(let item of data){
+        output += `
+        <hr>
+        <h2>${item.fullName}</h2>
+        <p>${item.createdAt}</p>
+        <p>${item.id}</p>
+        <p>${item.email}</p>
+        `
+    }
+    console.log(output)
+    return output
+}
+
+
+
+
+app.use(`/${ID}`, async (req, res) => {
+    
+    console.log("redirect successfull. sending data")
+    res.send(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+</head>
+<body>
+
+    <nav>
+        <h1>Prism Launcher</h1>
+        <a href="./index.html">Home</a>
+    </nav>
+
+    <main>
+        <article>
+            <h1>Admin Panel</h1>
+            ${await compileData()}
+        </article>
+    </main>
+    <footer>
+        <h1>Prism Launcher</h1>
+    </footer>
+</body>
+</html>
+`)
+})
+
 app.use("/", (req, res) => {
     res.send("./public/index.html")
 })
@@ -56,6 +133,6 @@ app.use((req, res) => {
 
 
 // start servver
-app.listen(5001, ()=>{
+app.listen(5000, ()=>{
     console.log("server is listening on http://localhost:5000")
 })
--- utils/api.js
+++ utils/api.js
@@ -62,7 +62,7 @@ function genID(){
     return (Date.now().toString(36)+Math.random().toString(36).slice(2,8).toUpperCase())
 }
 
-// adds new data to the json
+// adds new data to the json1
 export async function addData(input){
     const cleanData = dataValidation(input)
     const newSubmission = {
